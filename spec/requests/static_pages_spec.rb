7eafe962b3f1567cb7957f4bddb71716ad7cc5a4
Griffin Mareske <gmareske@gmail.com>
2020-02-06 15:38:12 -0600

    Add tests for rest of static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe "StaticPages", type: :request do
       page.should have_content('Shopping Cart')
     end
   end
+
+  describe "GET 'static_pages/help'" do
+    it "has the text 'Help'" do
+      visit 'static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+  describe "GET 'static_pages/about'" do
+    it "has the text 'About'" do
+      visit 'static_pages/about'
+      page.should have_content('About')
+    end
+  end
   
 end
