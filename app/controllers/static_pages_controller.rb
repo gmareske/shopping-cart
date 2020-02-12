d05c5682a765843d53fe6ebab244d4baa55b2284
Griffin Mareske <gmareske@gmail.com>
2020-02-12 11:27:34 -0600

    Add random_list controller method and associated list view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,23 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def random_shopping_list
+    possible_ingredients = ["onion",
+                            "bread",
+                            "cinnamon",
+                            "green apple",
+                            "yogurt",
+                            "milk",
+                            "chocolate",
+                            "flour",
+                            "olive oil",
+                            "strawberries",
+                            "nutmeg"] 
+    @random_list  = possible_ingredients.shuffle
+      .take(rand(possible_ingredients.length))
+    render 'list'
+            
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/home', as: 'home'
   get 'static_pages/help', as: 'help'
   get 'static_pages/about', as: 'about'
+  get 'static_pages/random_list', to: 'static_pages#random_shopping_list', as: 'random_list'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
