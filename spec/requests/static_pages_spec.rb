68ab0fcb79af2c65e2d055d6486b9fbd24b4812a
Griffin Mareske <gmareske@gmail.com>
2020-02-11 19:06:40 -0600

    Add test for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,26 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-
-  describe "GET 'static_pages/home'" do
+  describe "Home page" do
     it "has the text 'Shopping Cart'" do
-      visit 'static_pages/home'
+      visit home_path
       page.should have_content('Shopping Cart')
     end
+    
   end
 
-  describe "GET 'static_pages/help'" do
-    it "has the text 'Help'" do
-      visit 'static_pages/help'
-      page.should have_content('Help')
-    end
-  end
-
-  describe "GET 'static_pages/about'" do
+  describe "About page" do
     it "has the text 'About'" do
-      visit 'static_pages/about'
+      visit about_path
       page.should have_content('About')
     end
   end
-  
+
+  describe "Help page" do
+    it "has the text 'Help'" do
+      visit help_path
+      page.should have_content('Help')
+    end
+  end
 end
