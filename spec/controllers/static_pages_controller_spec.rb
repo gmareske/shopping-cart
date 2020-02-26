c9772e695dbc7c03dcc93f60cc23bf4649b0de23
Griffin Mareske <gmareske@gmail.com>
2020-02-25 22:45:17 -0600

    Add new tests for the dynamic Ruby content

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,26 +1,43 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe StaticPagesController do
+RSpec.describe StaticPagesController do
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
-      response.should be_success
+      response.should be_successful
     end
   end
 
   describe "GET 'help'" do
     it "returns http success" do
       get 'help'
-      response.should be_success
+      response.should be_successful
     end
   end
 
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
-      response.should be_success
+      response.should be_successful
     end
   end
 
+  describe "GET 'random_list'" do
+    it "returns https success" do
+      get 'random_shopping_list'
+      response.should be_successful
+    end
+
+    it "should generate a list of random ingredients" do
+      get 'random_shopping_list'
+      assigns(:random_list).should_not be_nil
+    end
+
+    it "Should render the random_shopping_list template" do
+      get 'random_shopping_list'
+      response.should render_template 'list'
+    end
+  end
+  
 end
