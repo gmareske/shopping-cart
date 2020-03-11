30d80d1ac6e6740664cda4b151cfef27f271d33a
Griffin Mareske <gmareske@gmail.com>
2020-03-10 21:30:11 -0500

    Change site root to recipe resource, update header, remove home pg

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root 'recipes#index'
   get 'static_pages/home', as: 'home'
   get 'static_pages/help', as: 'help'
   get 'static_pages/about', as: 'about'
