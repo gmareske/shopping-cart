e70d099c9722554a3c5ab21eb8443d7c76481c68
Griffin Mareske <gmareske@gmail.com>
2020-03-10 21:28:20 -0500

    Add index and show pages to Recipes resource

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,20 @@
 class RecipesController < ApplicationController
   def index
-    default_id = User.find(1).id
+    default_user = User.find(1)
     recipes_list = []
+
     # find all recipes that match the default user id
     # add them to the return result id
-    @recipes = Recipe.where(user_id=default_id).find_each do |r|
+    # these are public recipes that everyone should be able to see
+    Recipe.where(user: default_user).find_each do |r|
       recipes_list.append(r)
     end
+    @recipes = recipes_list
   end
   
   def show
+    @recipe = Recipe.find(params[:id])
+    @measurements = @recipe.measurements
   end
   
   def new
