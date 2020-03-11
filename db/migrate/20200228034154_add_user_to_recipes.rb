a63b7957953b42fedd8b6e28bede741ccfc2b490
Griffin Mareske <gmareske@gmail.com>
2020-03-11 11:40:57 -0500

    Switch up to add_foreign_key instead of add_column

--- db/migrate/20200228034154_add_user_to_recipes.rb
+++ db/migrate/20200228034154_add_user_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddUserToRecipes < ActiveRecord::Migration[5.2]
   def change
-    add_column :recipes, :user_id, :integer
+    add_foreign_key :recipes, :users, column: :user_id
   end
 end
--- db/migrate/20200228035122_add_recipe_and_ingredient_to_measurement.rb
+++ db/migrate/20200228035122_add_recipe_and_ingredient_to_measurement.rb
@@ -1,6 +1,6 @@
 class AddRecipeAndIngredientToMeasurement < ActiveRecord::Migration[5.2]
   def change
-    add_column :measurements, :recipe_id, :integer
-    add_column :measurements, :ingredient_id, :integer
+    add_foreign_key :measurements, :recipes, column: :recipe_id
+    add_column :measurements, :ingredients, column: :ingredient_id
   end
 end
