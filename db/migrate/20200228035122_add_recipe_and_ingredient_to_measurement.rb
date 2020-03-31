e32dab4b2fd4a6c3bb4b1dccaa518be94784604a
Griffin Mareske <gmareske@gmail.com>
2020-03-31 12:31:27 -0500

    Add foreign keys to measurements

--- db/migrate/20200228035122_add_recipe_and_ingredient_to_measurement.rb
+++ db/migrate/20200228035122_add_recipe_and_ingredient_to_measurement.rb
@@ -1,6 +1,6 @@
 class AddRecipeAndIngredientToMeasurement < ActiveRecord::Migration[5.2]
   def change
     add_foreign_key :measurements, :recipes, column: :recipe_id
-    add_column :measurements, :ingredients, column: :ingredient_id
+    add_foreign_key :measurements, :ingredients, column: :ingredient_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_161052) do
+ActiveRecord::Schema.define(version: 2020_03_11_163911) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
