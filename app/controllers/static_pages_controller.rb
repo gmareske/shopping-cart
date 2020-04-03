70316052c6c36a98464ce870f1631f7d5f343285
Griffin Mareske <gmareske@gmail.com>
2020-04-02 23:44:19 -0500

    Seed Random List page with data from db

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,19 +6,11 @@ class StaticPagesController < ApplicationController
   end
 
   def random_shopping_list
-    possible_ingredients = ["onion",
-                            "bread",
-                            "cinnamon",
-                            "green apple",
-                            "yogurt",
-                            "milk",
-                            "chocolate",
-                            "flour",
-                            "olive oil",
-                            "strawberries",
-                            "nutmeg"] 
-    @random_list  = possible_ingredients.shuffle
-      .take(rand(possible_ingredients.length))
+    # get 1-10 ingredients from the database
+    @random_list  = Ingredient.all.map{|i| i.name}.shuffle.take(rand(1..10))
+    # then for each ingredient we have, associate it with a random measurement
+    @measurement_list = @random_list.map{|x| Measurement.all.shuffle.first.amount}
+
     render 'list'
   end
 end
