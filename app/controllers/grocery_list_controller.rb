a9db2e4d4ec6a663c71c784c918ceaa9068617db
Griffin Mareske <gmareske@gmail.com>
2020-04-07 11:39:52 -0500

    whitespace

--- app/controllers/grocery_list_controller.rb
+++ app/controllers/grocery_list_controller.rb
@@ -7,6 +7,6 @@ class GroceryListController < ApplicationController
     params.permit(recipe_list: [])
     recipe_ids = params[:recipe_list].keys.map {|k| k.to_i }
     @measurements = recipe_ids.collect {|id| Measurement.where(recipe_id: id)}.flatten
-
+    
   end
 end
