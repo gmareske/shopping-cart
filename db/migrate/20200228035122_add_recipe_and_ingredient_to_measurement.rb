25ad8e441cd6ce97db1d2b8b16e309eedb47e816
Griffin Mareske <gmareske@gmail.com>
2020-04-14 11:10:44 -0500

    Add references to measurements migration

--- db/migrate/20200228035122_add_recipe_and_ingredient_to_measurement.rb
+++ db/migrate/20200228035122_add_recipe_and_ingredient_to_measurement.rb
@@ -1,5 +1,8 @@
 class AddRecipeAndIngredientToMeasurement < ActiveRecord::Migration[5.2]
   def change
+    add_reference :measurements, :recipe, table: :recipes
+    add_reference :measurements, :ingredient, table: :ingredients
+
     add_foreign_key :measurements, :recipes, column: :recipe_id
     add_foreign_key :measurements, :ingredients, column: :ingredient_id
   end
