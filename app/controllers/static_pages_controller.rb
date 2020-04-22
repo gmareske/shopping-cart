06308b8faa3765d0e3fd5f55318804940013a431
Griffin Mareske <gmareske@gmail.com>
2020-04-22 14:24:15 -0500

    Modify random list function to use new partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,9 +7,12 @@ class StaticPagesController < ApplicationController
 
   def random_shopping_list
     # get 1-10 ingredients from the database
-    @random_list  = Ingredient.all.map{|i| i.name}.shuffle.take(rand(1..10))
+    #@random_list  = Ingredient.all.map{|i| i.name}.shuffle.take(rand(1..10))
     # then for each ingredient we have, associate it with a random measurement
-    @measurement_list = @random_list.map{|x| Measurement.all.shuffle.first.amount}
+    #@measurement_list = @random_list.map{|x| Measurement.all.shuffle.first.amount}
+
+    # get 1-10 random measurements from the database
+    @measurements = Measurement.all.shuffle.take(rand(1..10))
 
     render 'list'
   end
